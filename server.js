51982d5cbd7119187880c07e776cc4dbdf6482a1
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-04-02 18:06:42 -0500

    finished fs.readFile

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ var express = require("express");
 var path = require("path");
 // import fs from the node library
 var fs = require("fs");
-// import db.json file into the server.js file to reference
-var notesDB = require("./develop/db/db.json");
-//console.log(json)
+// import db.json file into the server.js file to reference and use fs readFileSync to read contents of the file
+var data = fs.readFileSync("./db/db.json", "utf8");
+console.log(data);
+// parse the json file
+var notesDB = JSON.parse(data);
+console.log(typeof(notesDB));
+// since notesDB is an object, we need to turn it into an array of objects
+var finalNotesDB = Object.keys(notesDB).map(i => notesDB[i])
+console.log(finalNotesDB);
+
 
 // instantiate a new express app utilizing the express() method
 var app = express();
@@ -16,31 +23,32 @@ var PORT = 8113;
 // middleware will parse the request string and convert the request to a json object that will be referenced later as req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// must reference the public folder for the front end stuff
-app.use(express.static("develop/public"));
+// must reference the public folder for the front end stuff- you have a static folder that does not change where the public facing files lives to interact with the user
+app.use(express.static("public"));
 
 // create the HTML get routes
     // client requests an html page on the root route
     // this is the root route - returns the index.html webpage
     app.get("/", function(req, res) {
         // utilize the res.sendFile() method which transfer the given file path and sets the content-type response HTTP header to the appropriate header (example- sends the html path to the browser)
-        res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+        res.sendFile(path.join(__dirname, "/public/index.html"));
     });
         // then use path.join to combine the directory path of the executing script to the .html file that we want to send to the client      
     // return the notes.html webpage
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+        res.sendFile(path.join(__dirname, "/public/notes.html"));
     });
     // return a catch all to the index.html page in case a user types the wrong link
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+        res.sendFile(path.join(__dirname, "/public/index.html"));
     });
 
 // create the API get routes
     // set up the notes get route 
     app.get("/api/notes", function(req, res) {
         // response needs to access the notesDB variable in order to send a response
-        res.json(notesDB);
+        res.json(data);
+        //console.log(finalNotesDB);
     });
 
 // create the API POST routes
@@ -49,15 +57,15 @@ app.use(express.static("develop/public"));
         notesDB.push(req.body);
         //notesDB.push("dinner", "pasta")
         // add the new note to the db.json file
-        fs.readFile("./develop/db/db.json", "utf-8", function (error){
-            req.body.push(notesDB);
+        fs.writeFile("./db/db.json", JSON.stringify(notesDB) + "\n", function(error){
+            if (error){
+                console.log("Your note did not save");
+            }
         })
-        fs.writeFile("./develop/db/db.json", JSON.stringify(notesDB), function(error){
-          req.body
-      })
           // return the new note to the client
-          res.json(notesDB);
+          res.json(finalNotesDB);
     })
+
 
 // create the API delete routes
 
